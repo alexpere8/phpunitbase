14e0081cf12c02207de3e297b100272af5ddb3cd
Alex <106488170+alexpere8@users.noreply.github.com>
2022-07-17 19:11:09 +0300

    Add files via upload

---
+++ DataTest.php
@@ -0,0 +1,38 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3]
+        ];
+    }
+
+    /**
+     * @dataProvider additionProviderB
+     */
+    public function testAddB($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a . $b);
+    }
+
+    public function additionProviderB()
+    {
+        return [
+            ['one', 'two', 'onetwo']
+        ];
+    }
+}
